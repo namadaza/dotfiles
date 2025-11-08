b7e9c1df8ae55f70235a2692cf3cc3caf1ac0ae1
Aman Azad <aman.s.azad@gmail.com>
2025-11-08 07:06:18 -0800

    ensure all scripts are executable

--- scripts/sketchybar-update-config.sh
+++ scripts/sketchybar-update-config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cp -r -f "$SCRIPT_DIR/../sketchybar"/* ~/.config/sketchybar/
+cp -r -f -p "$SCRIPT_DIR/../sketchybar"/* ~/.config/sketchybar/
+# Ensure all plugin scripts are executable
+chmod +x ~/.config/sketchybar/plugins/*.sh
 sketchybar --reload
