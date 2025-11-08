44e2f71e6d9e1febf28865da38a919d785112ed9
Aman Azad <aman.s.azad@gmail.com>
2025-11-08 06:52:43 -0800

    update battery icons

--- sketchybar/plugins/battery.sh
+++ sketchybar/plugins/battery.sh
@@ -8,19 +8,31 @@ if [ "$PERCENTAGE" = "" ]; then
 fi
 
 case "${PERCENTAGE}" in
-  9[0-9]|100) ICON="▊"
+  100) ICON="󰁹"
   ;;
-  [6-8][0-9]) ICON="▋"
+  9[0-9]) ICON="󰂂"
   ;;
-  [3-5][0-9]) ICON="▌"
+  8[0-9]) ICON="󰂁"
   ;;
-  [1-2][0-9]) ICON="▎"
+  7[0-9]) ICON="󰂀"
   ;;
-  *) ICON="▏"
+  6[0-9]) ICON="󰁿"
+  ;;
+  5[0-9]) ICON="󰁾"
+  ;;
+  4[0-9]) ICON="󰁽"
+  ;;
+  3[0-9]) ICON="󰁼"
+  ;;
+  2[0-9]) ICON="󰁻"
+  ;;
+  1[0-9]) ICON="󰁺"
+  ;;
+  *) ICON="󰁺"
 esac
 
 if [[ "$CHARGING" != "" ]]; then
-  ICON="⚡"
+  ICON="󰂄"
 fi
 
 # The item invoking this script (name $NAME) will get its icon and label
