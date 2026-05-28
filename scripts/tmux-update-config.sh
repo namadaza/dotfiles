134c28d1f08a4e41570b9499237b333511a26580
Aman Azad <aman.s.azad@gmail.com>
2026-05-28 09:15:07 -0700

    update tmux script to pull in plugin git submodules

--- scripts/tmux-update-config.sh
+++ scripts/tmux-update-config.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+REPO_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
+
+git -C "$REPO_ROOT" submodule update --init --recursive -- tmux/plugins/tmux-resurrect
+
 cp -f "$SCRIPT_DIR/../tmux/.tmux.conf" ~/.tmux.conf
 mkdir -p ~/.tmux/plugins
 cp -r -f "$SCRIPT_DIR/../tmux/plugins"/* ~/.tmux/plugins/
 tmux source ~/.tmux.conf
-
